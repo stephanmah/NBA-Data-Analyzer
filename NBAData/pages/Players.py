8a31e388e1a7bc0369eebc3ca344f225fda69daa
Stephan Mah <35859912+stephanmah@users.noreply.github.com>
2024-07-08 16:57:42 -0400

    Update Players.py

--- NBAData/pages/Players.py
+++ NBAData/pages/Players.py
@@ -27,13 +27,13 @@ player_stats = player_info.choose_player_stats(player_select_box,PerMode,season_
 
 
 player_stats_df = pd.DataFrame(player_stats)
-player_sorted_by_year = player_stats_df.sort_values(by="SEASON_ID", ascending = False)
+#player_sorted_by_year = player_stats_df.sort_values(by="SEASON_ID", ascending = False)
 
 st.header(player_select_box)
 
-player_stats_table = st.dataframe(player_sorted_by_year, hide_index= True, use_container_width=True, column_order= ("SEASON_ID","TEAM_ABBREVIATION","PLAYER_AGE","GP","GS","MIN","PTS","FGM","FGA","FG_PCT","FG3M","FG3A","FG3_PCT","FTM","FTA","FT_PCT","OREB","DREB","REB","AST","STL","BLK","TOV","PF"),column_config= ({"SEASON_ID": "Season","TEAM_ABBREVIATION": "Team","PLAYER_AGE" : "Age","GP":"Games Played","GS" : "Games Started","MIN": 'Minutes Played',"FGM" : 'Field Goals Made',"FGA": "Field Goals Attempted","FG_PCT" :"Field Goal Percentage","FG3M" : "3 Pointers Made","FG3A" : "Field Goals Attempted","FG3_PCT": "3 Point %","FTM" : "Free Throws Made","FTA":"Free Throws Attempted","FT_PCT" :"Free Throw %","OREB": "Offensive Rebounds","DREB": "Defensive Rebounds","REB":"Rebounds","AST": "Assists","STL": "Steals","BLK":"Blocks","TOV" : "Turnover","PF":"Personal Fouls","PTS": "Points"}))
+player_stats_table = st.dataframe(player_stats_df, hide_index= True, use_container_width=True, column_order= ("SEASON_ID","TEAM_ABBREVIATION","PLAYER_AGE","GP","GS","MIN","PTS","FGM","FGA","FG_PCT","FG3M","FG3A","FG3_PCT","FTM","FTA","FT_PCT","OREB","DREB","REB","AST","STL","BLK","TOV","PF"),column_config= ({"SEASON_ID": "Season","TEAM_ABBREVIATION": "Team","PLAYER_AGE" : "Age","GP":"Games Played","GS" : "Games Started","MIN": 'Minutes Played',"FGM" : 'Field Goals Made',"FGA": "Field Goals Attempted","FG_PCT" :"Field Goal Percentage","FG3M" : "3 Pointers Made","FG3A" : "Field Goals Attempted","FG3_PCT": "3 Point %","FTM" : "Free Throws Made","FTA":"Free Throws Attempted","FT_PCT" :"Free Throw %","OREB": "Offensive Rebounds","DREB": "Defensive Rebounds","REB":"Rebounds","AST": "Assists","STL": "Steals","BLK":"Blocks","TOV" : "Turnover","PF":"Personal Fouls","PTS": "Points"}))
 
-print(player_stats_table)
+
 
 
 
